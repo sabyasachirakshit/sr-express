9b191205c8f9b854a8d966eea7b64ae7ecb03afd
sabyasachirakshit <sabyasachirakshitacs19@gmail.com>
2024-05-30 19:10:44 +0530

    env  created

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
-// config.js
 module.exports = {
-    MONGO_URI: 'mongodb+srv://sr19clon3:DgqyIqDUTC5405yR@cluster0.ds9qbjs.mongodb.net/',
-    jwtSecret: '94d9e991a167e5f40e8cc15620bbc4f66a9e090b8b0ee1006da8b2fb0b96d212'
-  };
-  
+  MONGO_URI: process.env.MONGO_URI || 'mongodb://localhost:27017/',
+  jwtSecret: process.env.JWT_SECRET || 'your_default_jwt_secret'
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,29 +1,4 @@
-// // middleware/auth.js
-// const jwt = require('jsonwebtoken');
-// const config = require('config');
-
-// module.exports = function (req, res, next) {
-//   // Get token from header
-//   const token = req.header('Authorization').replace('Bearer ', '');
-
-//   // Check if no token
-//   if (!token) {
-//     return res.status(401).json({ msg: 'No token, authorization denied' });
-//   }
-
-//   // Verify token
-//   try {
-//     console.log("THIS IS TOKEN:",token);
-//     const decoded = jwt.verify(token, config.get('jwtSecret'));
-//     req.user = decoded.user;
-//     next();
-//   } catch (err) {
-//     res.status(401).json({ msg: 'Token is not valid' });
-//   }
-// };
-
-
-
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 const config = require('../config');
 
